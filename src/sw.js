c63bd2f37a5b51d46f60dc10ad53a7b3942be59e
rjbultitude <richard.bultitude@gmail.com>
2020-02-15 14:47:04 +0000

    configure sw

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,15 @@
+const DYNAMIC_STATIC_NAME = 'dyn-v1a';
+const btnAdd = document.getElementById('add');
+let deferredPrompt;
+
+self.isOnlyIfCached = function isOnlyIfCached(event) {
+  if (event.request !== undefined) {
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+      return true;
+    }
+  }
+};
+
 self.addEventListener('install', (installEvent) => {
   return;
 });
@@ -5,3 +17,55 @@ self.addEventListener('install', (installEvent) => {
 self.addEventListener('activate', (activateEvent) => {
   return self.clients.claim();
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the network
+    fetch(event.request)
+      .then(function(res) {
+        return caches.open(DYNAMIC_STATIC_NAME)
+          .then(function(cache) {
+            // Put in cache if succeeds
+            cache.put(event.request.url, res.clone());
+            return res;
+          })
+      })
+      .catch(function (err) {
+          // Fallback to cache
+          return caches.match(event.request)
+            .then(function(res) {
+              if (res === undefined) {
+              // get and return the offline page
+            }
+            return res;
+        })
+      })
+  );
+});
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    showInstallPromotion();
+});
+
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+
+  window.addEventListener('appinstalled', (evt) => {
+    console.log('a2hs installed');
+  });
