1c7f2959d02d52adc70663a1381dd7808b413cc9
rjbultitude <richard.bultitude@gmail.com>
2020-02-15 15:03:43 +0000

    sw updates

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 ((win, doc, nav) => {
+  const btnAdd = document.getElementById('add');
   const detectFeatures = (registration) => {
     return {
       'Offline Capabilities': 'caches' in win,
@@ -58,9 +59,38 @@
             .then((registration) => {
               const pwaFeatures = detectFeatures(registration);
               updateUserInterface(pwaFeatures);
+              // navigator.serviceWorker.controller.postMessage();
             });
       }
       updateUserInterface({'Service Workers Not Supported': false});
     }, 500);
+  });
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    console.log('beforeinstallprompt fired', deferredPrompt);
+    // showInstallPromotion();
+});
+
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+
+  window.addEventListener('appinstalled', (evt) => {
+    console.log('a2hs installed');
   });
 })(window, document, navigator);
